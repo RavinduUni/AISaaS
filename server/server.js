788fee4982af5e88509c45e93a99b14033ce869f
RavinduUni <fc115680@foc.sjp.ac.lk>
2026-07-15 20:28:58 +0530

    'create-db-config'

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send(`<h1>AI SaaS Server Running</h1><p>Server is running on port ${PORT}</p>`);
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
